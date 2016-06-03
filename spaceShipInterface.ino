2428cf6063fab38f10d802c73d0ac4a4d22ccc08
Cristian Robles <cristianrobles175@gmail.com>
2016-06-02 20:33:22 -0700

    Both Flash Lights Blink!!!

---
+++ spaceShipInterface.ino
@@ -0,0 +1,31 @@
+int cambiaEstado = 0;
+
+void setup() {
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(2, INPUT);
+}
+
+void loop() {
+  cambiaEstado = digitalRead(2);
+
+  //comment
+
+  
+if(cambiaEstado == LOW){
+  digitalWrite(3, HIGH);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+  }else{
+    digitalWrite(3, LOW);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
+
+  delay(250);
+  digitalWrite(4, LOW);
+  delay(350);
+  digitalWrite(5, LOW);
+  delay(250);
+    }
+}
